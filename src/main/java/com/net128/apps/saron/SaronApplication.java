6f8453df11c2943f79d1eaa03d2fa3844163f2af
Michael Seger <michael@michaels-imac-1.lan>
2023-01-29 10:26:30 +0100

    ...

---
+++ src/main/java/com/net128/apps/saron/SaronApplication.java
@@ -0,0 +1,13 @@
+package com.net128.apps.saron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/net128/apps/saron/SaronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net128.apps.saron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
