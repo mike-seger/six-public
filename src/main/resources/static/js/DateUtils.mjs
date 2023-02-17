c37fb4bfe64e2e935f9e1e36e9f8c8df413ddc74
michael <michael@mac>
2023-02-17 09:22:31 +0100

    static cache initialization

--- src/main/resources/static/js/DateUtils.mjs
+++ src/main/resources/static/js/DateUtils.mjs
@@ -25,8 +25,6 @@ function plusDays(isoDateStr, days, onlyWeekdays) {
 	let isBankHoliday = onlyWeekdays
 	if(onlyWeekdays === undefined || ! typeof isBankHoliday === 'function') {
 		isBankHoliday = (date) => onlyWeekdays?!isWeekDay(localDate(date)):false
-	} else {
-		console.log("")
 	}
 	if(typeof isoDateStr.getMonth === 'function')
 		isoDateStr = isoDate(isoDateStr)
@@ -44,7 +42,6 @@ function plusDays(isoDateStr, days, onlyWeekdays) {
 
 function plusSwissWorkingDays(isoDateStr, days) {
 	const result = plusDays(isoDateStr, days, isSwissBankHoliDay)
-	console.log(isoDateStr, days, result)
 	return result
 }
 
@@ -76,12 +73,12 @@ function getPrevPeriod(date, period) {
 }
 
 /*
-https://stackoverflow.com/questions/1284314/easter-date-in-javascript
-https://www.contextures.com/exceleastercalculation.html
-Laurent Longre Algorithm
-Accuracy: This algorithm is definitely worthy of mention. It is the shortest of all to implement, and it is the fastest running of the group by roughly a factor of 2. It is in 100% agreement with both the U.S. Naval Observatory (USNO) and Astronomical Society of Southern Australia (ASSA) calculations from 1900 through 6553. After 6553, the algorithm returns the #INVALID! error.
-Speed: The average time to execute was 4.85675 microseconds (4.85675E-06 seconds).
-Note: For years prior to 1900, or for overflow errors, see suggestions below the code
+	https://stackoverflow.com/questions/1284314/easter-date-in-javascript
+	https://www.contextures.com/exceleastercalculation.html
+	Laurent Longre Algorithm
+	Accuracy: This algorithm is definitely worthy of mention. It is the shortest of all to implement, and it is the fastest running of the group by roughly a factor of 2. It is in 100% agreement with both the U.S. Naval Observatory (USNO) and Astronomical Society of Southern Australia (ASSA) calculations from 1900 through 6553. After 6553, the algorithm returns the #INVALID! error.
+	Speed: The average time to execute was 4.85675 microseconds (4.85675E-06 seconds).
+	Note: For years prior to 1900, or for overflow errors, see suggestions below the code
 */
 function easterDate(Y) {
   let M=3, G= Y % 19+1, C= ~~(Y/100)+1, L=~~((3*C)/4)-12,
@@ -123,22 +120,85 @@ function cacheSwissHolidays() {
 	return cache
 }
 
-let swissBankHolidayCache = cacheSwissHolidays()
+const swissBankHolidayRawCache = [ //cacheSwissHolidays()
+	{1990:["01-01","01-02","04-13","04-16","05-01","05-24","06-04","08-01","12-25","12-26"]},
+	{1991:["01-01","01-02","03-29","04-01","05-01","05-09","05-20","08-01","12-25","12-26"]},
+	{1992:["01-01","01-02","04-17","04-20","05-01","05-28","06-08","08-01","12-25","12-26"]},
+	{1993:["01-01","01-02","04-09","04-12","05-01","05-20","05-31","08-01","12-25","12-26"]},
+	{1994:["01-01","01-02","04-01","04-04","05-01","05-12","05-23","08-01","12-25","12-26"]},
+	{1995:["01-01","01-02","04-14","04-17","05-01","05-25","06-05","08-01","12-25","12-26"]},
+	{1996:["01-01","01-02","04-05","04-08","05-01","05-16","05-27","08-01","12-25","12-26"]},
+	{1997:["01-01","01-02","03-28","03-31","05-01","05-08","05-19","08-01","12-25","12-26"]},
+	{1998:["01-01","01-02","04-10","04-13","05-01","05-21","06-01","08-01","12-25","12-26"]},
+	{1999:["01-01","01-02","04-02","04-05","05-01","05-13","05-24","08-01","12-25","12-26"]},
+	{2000:["01-01","01-02","04-21","04-24","05-01","06-01","06-12","08-01","12-25","12-26"]},
+	{2001:["01-01","01-02","04-13","04-16","05-01","05-24","06-04","08-01","12-25","12-26"]},
+	{2002:["01-01","01-02","03-29","04-01","05-01","05-09","05-20","08-01","12-25","12-26"]},
+	{2003:["01-01","01-02","04-18","04-21","05-01","05-29","06-09","08-01","12-25","12-26"]},
+	{2004:["01-01","01-02","04-09","04-12","05-01","05-20","05-31","08-01","12-25","12-26"]},
+	{2005:["01-01","01-02","03-25","03-28","05-01","05-05","05-16","08-01","12-25","12-26"]},
+	{2006:["01-01","01-02","04-14","04-17","05-01","05-25","06-05","08-01","12-25","12-26"]},
+	{2007:["01-01","01-02","04-06","04-09","05-01","05-17","05-28","08-01","12-25","12-26"]},
+	{2008:["01-01","01-02","03-21","03-24","05-01","05-01","05-12","08-01","12-25","12-26"]},
+	{2009:["01-01","01-02","04-10","04-13","05-01","05-21","06-01","08-01","12-25","12-26"]},
+	{2010:["01-01","01-02","04-02","04-05","05-01","05-13","05-24","08-01","12-25","12-26"]},
+	{2011:["01-01","01-02","04-22","04-25","05-01","06-02","06-13","08-01","12-25","12-26"]},
+	{2012:["01-01","01-02","04-06","04-09","05-01","05-17","05-28","08-01","12-25","12-26"]},
+	{2013:["01-01","01-02","03-29","04-01","05-01","05-09","05-20","08-01","12-25","12-26"]},
+	{2014:["01-01","01-02","04-18","04-21","05-01","05-29","06-09","08-01","12-25","12-26"]},
+	{2015:["01-01","01-02","04-03","04-06","05-01","05-14","05-25","08-01","12-25","12-26"]},
+	{2016:["01-01","01-02","03-25","03-28","05-01","05-05","05-16","08-01","12-25","12-26"]},
+	{2017:["01-01","01-02","04-14","04-17","05-01","05-25","06-05","08-01","12-25","12-26"]},
+	{2018:["01-01","01-02","03-30","04-02","05-01","05-10","05-21","08-01","12-25","12-26"]},
+	{2019:["01-01","01-02","04-19","04-22","05-01","05-30","06-10","08-01","12-25","12-26"]},
+	{2020:["01-01","01-02","04-10","04-13","05-01","05-21","06-01","08-01","12-25","12-26"]},
+	{2021:["01-01","01-02","04-02","04-05","05-01","05-13","05-24","08-01","12-25","12-26"]},
+	{2022:["01-01","01-02","04-15","04-18","05-01","05-26","06-06","08-01","12-25","12-26"]},
+	{2023:["01-01","01-02","04-07","04-10","05-01","05-18","05-29","08-01","12-25","12-26"]},
+	{2024:["01-01","01-02","03-29","04-01","05-01","05-09","05-20","08-01","12-25","12-26"]},
+	{2025:["01-01","01-02","04-18","04-21","05-01","05-29","06-09","08-01","12-25","12-26"]},
+	{2026:["01-01","01-02","04-03","04-06","05-01","05-14","05-25","08-01","12-25","12-26"]},
+	{2027:["01-01","01-02","03-26","03-29","05-01","05-06","05-17","08-01","12-25","12-26"]},
+	{2028:["01-01","01-02","04-14","04-17","05-01","05-25","06-05","08-01","12-25","12-26"]},
+	{2029:["01-01","01-02","03-30","04-02","05-01","05-10","05-21","08-01","12-25","12-26"]},
+	{2030:["01-01","01-02","04-19","04-22","05-01","05-30","06-10","08-01","12-25","12-26"]},
+	{2031:["01-01","01-02","04-11","04-14","05-01","05-22","06-02","08-01","12-25","12-26"]},
+	{2032:["01-01","01-02","03-26","03-29","05-01","05-06","05-17","08-01","12-25","12-26"]},
+	{2033:["01-01","01-02","04-15","04-18","05-01","05-26","06-06","08-01","12-25","12-26"]},
+	{2034:["01-01","01-02","04-07","04-10","05-01","05-18","05-29","08-01","12-25","12-26"]},
+	{2035:["01-01","01-02","03-23","03-26","05-01","05-03","05-14","08-01","12-25","12-26"]},
+	{2036:["01-01","01-02","04-11","04-14","05-01","05-22","06-02","08-01","12-25","12-26"]},
+	{2037:["01-01","01-02","04-03","04-06","05-01","05-14","05-25","08-01","12-25","12-26"]},
+	{2038:["01-01","01-02","04-23","04-26","05-01","06-03","06-14","08-01","12-25","12-26"]},
+	{2039:["01-01","01-02","04-08","04-11","05-01","05-19","05-30","08-01","12-25","12-26"]}
+]
+
+function rawCache2Cache(rawCache) {
+	const cache = new Map()
+	rawCache.map(entry => 
+		cache.set(
+			String(Object.keys(entry)[0]), 
+			Object.values(entry)[0].map(dm => 
+				String(Object.keys(entry)[0])+"-"+dm))
+	)
+	return cache
+}
+const swissBankHolidayCache = rawCache2Cache(swissBankHolidayRawCache)
 
 function isSwissBankHoliDay(isoDate) {
 	let result = true
 	if(isWeekDay(localDate(isoDate))) {
 		const year = String(isoDate).substring(0,4)
 		let holidays = swissBankHolidayCache.get(year)
-		if(!holidays) cache.set(String(year), swissBankHolidaysIso(year))
+		if(!holidays) swissBankHolidayCache.set(String(year), swissBankHolidaysIso(year))
 		if(! swissBankHolidayCache.get(String(isoDate).substring(0,4)).includes(isoDate))
 			result = false
 	}
-	console.log(isoDate, result)
 	return result
 }
 
 function test() {
+	console.log(swissBankHolidayCache)
 	isSwissBankHoliDay("2023-04-07")
 	isSwissBankHoliDay("2023-04-10")
 	isSwissBankHoliDay("2023-05-18")
@@ -149,13 +209,5 @@ function test() {
 
 test()
 
-// console.log(
-// 	// easterDate(2023),easterDate(2024),easterDate(2025),easterDate(2026),easterDate(2027),
-// 	// "2023", swissBankHolidaysIso("2023"),
-// 	// "2024", swissBankHolidaysIso("2024"),
-// 	// "2025", swissBankHolidaysIso("2025"),
-// 	// "2026", swissBankHolidaysIso("2026"),
-// 	// "2027", swissBankHolidaysIso("2027")
-// )
-
-export { localDate, diffDays, plusDays, plusSwissWorkingDays, isoDate, getPrevPeriod }
+export { localDate, diffDays, plusDays, plusSwissWorkingDays, 
+	isSwissBankHoliDay, isoDate, getPrevPeriod }
